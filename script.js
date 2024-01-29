9d4849a6491c44e03d2e3554e7570cbeef618a29
Sky Fuzell-Casey <skyfc.work@gmail.com>
2024-01-29 14:23:39 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var likesOne = document.querySelector("#likeOne").innerText;
+console.log(likesOne);
+
+function addLikeOne() {
+    likesOne++;
+    document.querySelector("#likeOne").innerText = likesOne;
+    console.log(likesOne);
+}
+
+var likesTwo = document.querySelector("#likeTwo").innerText;
+console.log(likesTwo);
+
+function addLikeTwo() {
+    likesTwo++;
+    document.querySelector("#likeTwo").innerText = likesTwo;
+    console.log(likesTwo);
+}
+
+var likesThree = document.querySelector("#likeThree").innerText;
+console.log(likesThree);
+
+function addLikeThree() {
+    likesThree++;
+    document.querySelector("#likeThree").innerText = likesThree;
+    console.log(likesThree);
+}
